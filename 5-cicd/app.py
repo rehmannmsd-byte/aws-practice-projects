4dca71a03f9a80d481da7ab35ae4ee7cff63458c
rehmannmsd-byte <rehmannmsd@gmail.com>
2026-08-08 16:03:51 +0530

    Change home route message in app.py

--- 5-cicd/app.py
+++ 5-cicd/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "updated cicd pipeline working 🚀"
+    return "updated cicd pipeline 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
