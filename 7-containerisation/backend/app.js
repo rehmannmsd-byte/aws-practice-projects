8562ffae11b6408bbe6f9196cdf7897ade453bb5
Nonsyncc <rehmannmsd@gmail.com>
2026-08-09 10:30:17 +0530

    7-containerisation

---
+++ 7-containerisation/backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Node.js backend 🚀"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
